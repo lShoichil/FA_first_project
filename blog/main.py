af6b4ce4d4f8aa088f993ae56be3b5ce5b58529b
Shoichi <leningdenis@gmail.com>
2022-10-05 14:17:03 +1000

    add user

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,9 @@
+from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -16,8 +18,9 @@ def get_db():
         db.close()
 
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+@app.post('/blog', status_code=status.HTTP_201_CREATED,
+          tags=['blogs'])
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
@@ -25,18 +28,67 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[schemas.ShowBlog],
+          tags=['blogs'])
 def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}", status_code=200)
-def show(blog_id, response: Response, db: Session = Depends(get_db)):
+@app.get("/blog/{blog_id}", status_code=200,
+         response_model=schemas.ShowBlog, tags=['blogs'])
+def show_blog(blog_id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'Blog with id {blog_id} in not available')
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'detail': f'Blog with id {blog_id} in not available'}
     return blog
+
+
+@app.delete('/blog/{blog_id}', status_code=status.HTTP_204_NO_CONTENT,
+          tags=['blogs'])
+def delete_blog(blog_id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {blog_id} not found.')
+
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {'detail': f'Blog with id {blog_id} is deleted'}
+
+
+@app.put('/blog/{blog_id}', status_code=status.HTTP_202_ACCEPTED,
+          tags=['blogs'])
+def update_blog(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Blog with id {blog_id} not found.')
+
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
+@app.post('/user', response_model=schemas.ShowUser,
+          tags=['users'])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(name=request.name, email=request.email,
+                           password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+
+@app.get('/user/{user_id}', response_model=schemas.ShowUser,
+          tags=['users'])
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with id {user_id} is not available.')
+    return user
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
-
+from sqlalchemy.orm import relationship
 
 class Blog(Base):
     __tablename__ = 'blogs'
@@ -8,3 +8,12 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
